4d345a8e8a80a0b5780ecaf9b59501210e99e230
Fang Zhang <walter.zhangfang@gmail.com>
2024-07-18 21:43:34 +1200

    Initial website static files.

---
+++ text/brand.js
@@ -0,0 +1,13 @@
+module.exports = {
+  agency: {
+    name: 'Landhill Capital',
+    desc: 'Incubating profitable website & SaaS businesses',
+    prefix: 'Landhill',
+    footerText: 'Landhill Capital - All Rights Reserved',
+    logoText: 'Landhill Capital',
+    projectName: 'Landhill Capital',
+    url: 'luxireact.ux-maestro.com/agency',
+    img: '/static/images/landhill-logo-only-white.png',
+    notifMsg: 'Donec sit amet nulla sed arcu pulvinar ultricies commodo id ligula.'
+  }
+};
